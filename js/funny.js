12400170e721d55ac625cbaeec7c8e4950fdb86f
horangg <94329609+horangg@users.noreply.github.com>
2022-01-05 10:36:13 +0900

    최종

--- js/funny.js
+++ js/funny.js
@@ -6,10 +6,13 @@ const closenews = paper.querySelector(".x");
 const opennews = () => {
   paper.classList.remove("hidden");
   console.log('gkgk')
+  console.log(curIndex2);
 }
 const closednews = () => {
   paper.classList.add("hidden");
-  console.log('gkgk')
+  console.log('gkg')
+  slideList2.style.transform = "translate3d(-" + slideWidth2 + "px, 0px, 0px)";
+  curIndex2 = 0;
 }
 newsoverlay.addEventListener("click", closednews);
 closenews.addEventListener("click", closednews);
@@ -24,7 +27,7 @@ const slideLen2 = slideContents2.length; // slide length
 const slideWidth2 = 840; // slide width
 const slideSpeed2 = 300; // slide speed
 const startNum2 = 0; // initial slide index (0 ~ 4)
-slideList2.style.width = slideWidth2 * (slideLen2 + 18) + "px";
+slideList2.style.width = slideWidth2 * (slideLen2 + 21) + "px";
 // Copy first and last slide
 let firstChild2 = slideList2.firstElementChild;
 let lastChild2 = slideList2.lastElementChild;
@@ -42,6 +45,7 @@ slideBtnNext2.addEventListener('click', function () {
   if (curIndex2 <= slideLen2 - 1) {
     slideList2.style.transition = slideSpeed2 + "ms";
     slideList2.style.transform = "translate3d(-" + (slideWidth2 * (curIndex2 + 2)) + "px, 0px, 0px)";
+    console.log(curIndex2);
   }
   if (curIndex2 === slideLen2 - 1) {
     setTimeout(function () {
@@ -49,6 +53,7 @@ slideBtnNext2.addEventListener('click', function () {
       slideList2.style.transform = "translate3d(-" + slideWidth2 + "px, 0px, 0px)";
     }, slideSpeed2);
     curIndex2 = -1;
+    console.log(curIndex2);
   }
   curSlide2 = slideContents2[++curIndex2];
 });
@@ -57,6 +62,7 @@ slideBtnPrev2.addEventListener('click', function () {
   if (curIndex2 >= 0) {
     slideList2.style.transition = slideSpeed2 + "ms";
     slideList2.style.transform = "translate3d(-" + (slideWidth2 * curIndex2) + "px, 0px, 0px)";
+    console.log(curIndex2);
   }
   if (curIndex2 === 0) {
     setTimeout(function () {
@@ -64,6 +70,7 @@ slideBtnPrev2.addEventListener('click', function () {
       slideList2.style.transform = "translate3d(-" + (slideWidth2 * slideLen2) + "px, 0px, 0px)";
     }, slideSpeed2);
     curIndex2 = slideLen2;
+    console.log(curIndex2);
   }
   curSlide2.classList.remove('slide_active2');
   curSlide2 = slideContents2[--curIndex2];
